384208fabeb2b8f76fec97edf4c475326a2900d0
Jimmy Amash <jimmy@hellojumper.com>
2019-05-02 07:47:55 -0700

    URL update for link

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             <div className="row">
                 <Wrap type="image" size="col-md-12" color="transparent" title="false"/>
                 <Wrap type="headline" text="Jimmy Amash is a developer, product maker, aspiring polymath and mild yogi" size="col-md-12" color="transparent" title="true" />
-                <Wrap type="link" text="View Resume" icon="true" iconname="file" linkto="https://standardresume.co/api/gpdf?id=-K8C0JpRd8QYZXOxT-01&filename=james-amash-resume.pdf" size="col-md-12" color="transparent" title="false" />
+                <Wrap type="link" text="View Resume" icon="true" iconname="file" linkto="https://standardresume.co/jimmyamash" size="col-md-12" color="transparent" title="false" />
                 <Wrap type="social" size="col-md-12" color="white" title="false" />
                 <Wrap type="headline" text="Built with React" size="col-md-12" color="transparent" title="true" />
             </div>
