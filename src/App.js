2c98397f5c849fbe56cf4597433afa0d98a9b5aa
Jimmy Amash <jimmy@hellojumper.com>
2019-07-22 19:35:59 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ class App extends Component {
                 <Wrap type="image" size="col-md-12" color="transparent" title="false"/>
                 <Wrap type="headline" text="Jimmy Amash is a developer, product maker, aspiring polymath and mild yogi" size="col-md-12" color="transparent" title="true" />
                 <Wrap type="link" text="Currently @Acorns" icon="false" iconname="file" linkto="https://twitter.com/acorns" size="col-md-12" color="transparent" title="false" />
+                // <Wrap type="link" text="View Resume" icon="true" iconname="file" linkto="https://standardresume.co/jimmyamash" size="col-md-12" color="transparent" title="false" />
                 <Wrap type="social" size="col-md-12" color="white" title="false" />
                 <Wrap type="headline" text="Built with React" size="col-md-12" color="transparent" title="true" />
             </div>
